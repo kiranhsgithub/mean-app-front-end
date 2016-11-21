4b628e28649e393c4309eda983e8cdf616d2fc6c
kiranhsgithub <hskiran.bms@gmail.com>
2016-11-20 19:14:45 -0500

    front end changes for auth login

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -10,6 +10,6 @@ export function config ($logProvider, toastrConfig, $authProvider, API_URL) {
   toastrConfig.preventDuplicates = true;
   toastrConfig.progressBar = true;
 
-    $authProvider.signupUrl = API_URL + '/auth/register';
-    //$authProvider.signupUrl = API_URL + '/auth/register';
+    $authProvider.signupUrl = API_URL + 'auth/register';
+    $authProvider.loginUrl = API_URL + 'auth/login';
 }
